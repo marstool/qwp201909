5ff00809b960cec1b33fb54b14723c1f937a0c6c
Your Name <you@example.com>
2019-10-26 01:26:22 -0400

    init

---
+++ scripts.Hugo/content/_index.md
@@ -0,0 +1,8 @@
++++
+title = "  qwp.jjj123.com 秦偉平"
+description = "  qwp.jjj123.com 秦偉平  "
+weight = 20
++++
+
+
+
---
+++ sed.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+OOO=qwp201907
+AAA=qwp201909
+sed -i \
+    -e "s;${OOO}.jjj123.com;${AAA}.jjj123.com;g"  \
+    -e "s;marstool/${OOO}.git;marstool/${AAA}.git;g"  \
+    CNAME \
+    config \
+    scripts.Hugo/config.toml
+
